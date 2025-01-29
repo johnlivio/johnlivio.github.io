a2c1285b31c2018d313b49020ededcf71398e097
johnlivio <johnlheviu@gmail.com>
2025-01-29 19:45:19 -0300

    'final'

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
-//import Contact from './components/Contact';
 
 function App() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
